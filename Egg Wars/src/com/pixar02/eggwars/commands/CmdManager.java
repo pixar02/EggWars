0a56f52cd7b21d83efc0a9b238fedeffd7a90436
BuildTools <unconfigured@null.spigotmc.org>
2016-04-13 21:58:21 +0200

    new

---
+++ Egg Wars/src/com/pixar02/eggwars/commands/CmdManager.java
@@ -0,0 +1,15 @@
+package com.pixar02.eggwars.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CmdManager implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender s, Command command, String label, String[] args) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
