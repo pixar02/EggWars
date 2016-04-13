5d66ea4ca70bf958f47f30dcaf17efb41edb5e9c
BuildTools <unconfigured@null.spigotmc.org>
2016-04-13 22:38:25 +0200

    More Commands shells

---
+++ Egg Wars/src/com/pixar02/eggwars/commands/ListMapsCmd.java
@@ -0,0 +1,5 @@
+package com.pixar02.eggwars.commands;
+
+public class ListMapsCmd extends BaseCmd {
+
+}
---
+++ Egg Wars/src/com/pixar02/eggwars/commands/QuitGameCmd.java
@@ -0,0 +1,5 @@
+package com.pixar02.eggwars.commands;
+
+public class QuitGameCmd extends BaseCmd {
+
+}
